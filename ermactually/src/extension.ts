9057a957e49a29623e92bf8f9730f8d8fbe0c8e3
Juan Ferreira <nanoferreirag@gmail.com>
2025-11-17 22:39:31 -0500

    added input box

--- ermactually/src/extension.ts
+++ ermactually/src/extension.ts
@@ -13,10 +13,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('ermactually.ErmActually', () => {
+	const disposable = vscode.commands.registerCommand('ermactually.ErmActually', async () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ErmActually!');
+		const input = await vscode.window.showInputBox({prompt: "Type your name ma broda" });
+		vscode.window.showInformationMessage(`wassup ${input} how ya doin`);
 	});
 
 	context.subscriptions.push(disposable);
